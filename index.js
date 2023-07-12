f9471c5975e7c13aadbfa31d3cb8353c08fd6b89
Deckard Gerritsen <deckardger@gmail.com>
2023-07-12 15:12:43 +0800

    First Ball Movement

--- index.js
+++ index.js
@@ -3,7 +3,23 @@ const settings = {
   duration: 30, // Seconds
   paddingPercentage: 5, // Percent
   initialRadius: 10, // Percent
-  ballSize: 6, // Pixels
+  ballSize: 7, // Pixels
+  lineWidth: 1, // Pixels
+  initialStartAngle: Math.PI, // Radians
+};
+
+const drawPoint = (circle, point, angle) => {
+  const arcRaidus = (circle.clientWidth - settings.lineWidth) / 2;
+
+  const x = arcRaidus * Math.cos(angle);
+  const y = -(arcRaidus * Math.sin(angle));
+
+  point.style.left = `${
+    ((circle.clientWidth / 2 + x) / circle.clientWidth) * 100
+  }%`;
+  point.style.top = `${
+    ((circle.clientWidth / 2 + y) / circle.clientWidth) * 100
+  }%`;
 };
 
 const init = () => {
@@ -24,33 +40,46 @@ const init = () => {
     circle.classList.add("circle");
     circleContainer.style.width = `${diameterPercentage}%`;
     circleContainer.style.height = `${diameterPercentage}%`;
+    circleContainer.style.setProperty(
+      "--line-thickness",
+      `${settings.lineWidth}px`
+    );
 
     circleContainer.appendChild(circle);
     container.appendChild(circleContainer);
   }
+
+  for (let i = 0; i < 1; i++) {
+    const circle = document.getElementById("circle-1");
+
+    const point = document.createElement("div");
+    point.classList.add("point");
+    point.setAttribute("id", `point-${i + 1}`);
+    point.style.width = `${settings.ballSize}px`;
+    point.style.height = `${settings.ballSize}px`;
+
+    drawPoint(circle, point, settings.initialStartAngle);
+
+    circle.appendChild(point);
+  }
 };
 
+let startTime = new Date().getTime();
+
 const draw = () => {
-  const container = document.getElementById("points-container");
+  const currentTime = new Date().getTime();
+  const elapsedTime = (currentTime - startTime) / 1000;
 
-  const point = document.createElement("div");
-  point.classList.add("point");
-  point.style.width = `${settings.ballSize}px`;
-  point.style.height = `${settings.ballSize}px`;
+  const circle = document.getElementById("circle-1");
+  const point = document.getElementById("point-1");
 
-  const arcRaidus =
-    document.getElementById("circles-container").children[2].offsetWidth;
+  const velocity = 1;
 
-  const x = arcRaidus * Math.cos(Math.PI);
-  const y = arcRaidus * Math.sin(Math.PI);
+  drawPoint(circle, point, settings.initialRadius - elapsedTime * velocity);
 
-  // point.style.left = `${x}px`;
-  // point.style.top = `${y}px`;
+  console.log(elapsedTime);
 
-  console.log(x, y);
-  console.log(Math.cos(Math.PI), Math.sin(Math.PI));
-
-  container.appendChild(point);
+  requestAnimationFrame(draw);
 };
 
 init();
